16b40748d8d6d9e0ac098402160796f1e023a1ec
Aditya raj <aytida.dev@gmail.com>
2025-09-08 03:59:45 +0530

    test

--- client/src/components/alert.jsx
+++
@@ -1,8 +0,0 @@
-import "./alert.css";
-
-export default function Alert({ message, status }) {
-  if (!status) {
-    status = "danger";
-  }
-  return <div className={`alert ${status}`}>{message}</div>;
-}
